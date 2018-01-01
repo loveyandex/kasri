cbb581edafa52f8e501e3ec32da0fabc40d444a0
abvaalgit <abfzlabfzl@gmail.com>
2018-01-01 17:43:14 +0330

    s

--- src/main/java/com/jsoupway/Process.java
+++ src/main/java/com/jsoupway/Process.java
@@ -4,7 +4,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.Stack;
 
 public class Process {
@@ -14,12 +14,23 @@ public class Process {
         Stack<String> urls = new Stack<>();
         Document earth = null;
         try {
-            earth = Jsoup.connect("http://weather.uwyo.edu/cgi-bin/sounding?region=naconf&TYPE=TEXT%3ALIST&YEAR=2018&MONTH=01&FROM=all&TO=0100&STNM=71845").get();
-            earth.body().getElementsByTag("pre");
-
+            earth = Jsoup.connect("http://weather.uwyo.edu/cgi-bin/sounding?region=naconf&TYPE=TEXT%3ALIST&YEAR=2015&MONTH=03&FROM=all&TO=3112&STNM=76225").get();
+            String text=earth.body().getElementsByTag("pre").text();
+            File file=new File("e:\\data.dat");
+            file.createNewFile();
+            OutputStreamWriter inputStreamReader=new OutputStreamWriter(new FileOutputStream(file));
+            inputStreamReader.write(text);
+            inputStreamReader.flush();
+            inputStreamReader.close();
+            inputStreamReader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+    }
+
+
+    public static void main(String[] args) {
+        Process.getData();
     }
 }
