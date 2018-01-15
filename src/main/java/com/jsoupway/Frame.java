0f2b41d30ca7c3d1e20268b0b447969f935d5163
abvaalgit <abfzlabfzl@gmail.com>
2018-01-16 00:15:08 +0330

    add javafx ui for butifullize

--- src/main/java/com/jsoupway/Frame.java
+++ src/main/java/com/jsoupway/Frame.java
@@ -58,7 +58,6 @@ public class Frame extends Application {
             startProcess.start();
         });
         myStage.setScene(myScene);
-
         myStage.show();
     }
     Thread startProcess = new Thread(new Process());
