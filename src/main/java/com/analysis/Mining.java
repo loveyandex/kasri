03fbe83f95edff4b6959c81b74ca578b7a729e44
abvaalgit <abfzlabfzl@gmail.coom>
2018-03-11 21:29:45 +0330

    error

--- src/main/java/com/analysis/Mining.java
+++ src/main/java/com/analysis/Mining.java
@@ -21,7 +21,7 @@ public class Mining {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-  scanner   = new Scanner(reader);
+        scanner = new Scanner(reader);
     }
 
     public void readFile() throws IOException {
@@ -34,68 +34,68 @@ public class Mining {
             if (line.contains("<h2>") && line.contains("</h2>")) {
                 String shallode = line.replace("<h2>", "").replace("</h2>", "");
                 System.out.println(shallode);
-              getFileName = analysisHeaderGetFileName(shallode);
+                getFileName = analysisHeaderGetFileName(shallode);
             }
 
-            if (isItem(line,"<item1>")) {
+            if (isItem(line, "<item1>")) {
                 String subitem1 = "";
-                while (!( subitem1 = scanner.nextLine()).contains("</item1>")) {
+                while (!(subitem1 = scanner.nextLine()).contains("</item1>")) {
                     item1.append(subitem1 + "\r\n");
                 }
             } else
                 continue;
-            writeInFile(System.getProperty("user.dir")+"/assets",getFileName,item1);
+            writeInFile(System.getProperty("user.dir") + "/assets", getFileName, item1);
 
             System.out.println(item1);
 
             line = scanner.nextLine();
-            if (isItem(line,"<item2>")) {
+            if (isItem(line, "<item2>")) {
                 String subitem2 = "";
-                while (!( subitem2 = scanner.nextLine()).contains("</item2>"))
+                while (!(subitem2 = scanner.nextLine()).contains("</item2>"))
                     item2.append(subitem2 + "\n\r");
             } else
                 continue;
 
-            writeInFile(System.getProperty("user.dir")+"/assets",getFileName+"Item2",item2);
+            writeInFile(System.getProperty("user.dir") + "/assets", getFileName + "Item2", item2);
             System.out.println(item2);
             System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
 
-           if (scanner.hasNextLine())
-               readFile();
+            if (scanner.hasNextLine())
+                readFile();
 
         }
     }
 
-    private boolean writeInFile(String pathDirToSave, String childFileName,StringBuilder item1) throws IOException {
-        File dir=new File(pathDirToSave);
+    private boolean writeInFile(String pathDirToSave, String childFileName, StringBuilder item1) throws IOException {
+        File dir = new File(pathDirToSave);
         dir.mkdir();
-        File fileTosave = new File(dir,childFileName);
+        File fileTosave = new File(dir, childFileName);
         fileTosave.createNewFile();
-        OutputStreamWriter writer=new OutputStreamWriter(new FileOutputStream(fileTosave,false));
+        OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(fileTosave, false));
         writer.write(item1.toString());
         writer.flush();
         writer.close();
         return true;
     }
 
-    private boolean isItem(String line,String item) {
+    private boolean isItem(String line, String item) {
         return line.contains(item);
     }
 
-    private  String  analysisHeaderGetFileName(String title){
+    private String analysisHeaderGetFileName(String title) {
         String regexe;
 //        regexe = "[at]?[0-9]{1}[0-9]?Z";
         regexe = "[0-9]{1}[0-9]? [a-z|A-Z]* [0-9]{4}";
         // Step 1: Allocate a Pattern object to compile a regexe
         Pattern pattern = Pattern.compile(regexe, Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(title);
-        String date="";
+        String date = "";
         while (matcher.find())
-            date=matcher.group();
-        return date.replace(" ","_");
+            date = matcher.group();
+        return date.replace(" ", "_");
     }
 
-    private  void analysisItem1(String item1Content){
+    private void analysisItem1(String item1Content) {
 
     }
 
@@ -108,6 +108,5 @@ public class Mining {
             e.printStackTrace();
         }
     }
-
 
 }
