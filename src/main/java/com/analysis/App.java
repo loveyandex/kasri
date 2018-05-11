476238f3eb9b74015ed6b5ff70bc2ac2cc695e5d
abvaalgit <abfzlabfzl@gmail.com>
2018-05-12 00:44:53 +0430

    amin

--- src/main/java/com/analysis/App.java
+++ src/main/java/com/analysis/App.java
@@ -26,7 +26,7 @@ public class App extends Application {
     public void start(Stage primaryStage) throws Exception {
 
         primaryStage.setTitle("weather");
-        primaryStage.initStyle(StageStyle.UNDECORATED);
+
 
 
         GridPane rootNode = new GridPane();
