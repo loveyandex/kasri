53bbd8d835814738a9707d25394e1d12e2d5627b
abvaalgit <abfzlabfzl@gmail.com>
2018-08-07 17:06:47 +0430

    henzo socks!!!

--- src/main/java/com/amin/notification/Dm.java
+++ src/main/java/com/amin/notification/Dm.java
@@ -1,9 +1,10 @@
-package com.amin.notify;
 
-import com.amin.notification.Notification;
+
+package com.amin.notification;
+
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.scene.Scene;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -17,7 +18,9 @@ import java.nio.charset.StandardCharsets;
 /**
  * is created by aMIN on 8/7/2018 at 1:16 AM
  */
-public class Noti extends Application {
+public class Dm extends Application {
+
+
     public static void main(String[] args) {
         launch(args);
 
@@ -27,24 +30,12 @@ public class Noti extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         stage.setScene(new Scene(new VBox()));
-        Notification.Notifier instance = Notification.Notifier.INSTANCE;
-        instance.setPopupLifetime(Duration.millis(2000));
-        this.socket = new DatagramSocket(8687);
+        NotificationProgress.Notifier instance = NotificationProgress.Notifier.INSTANCE;
+        instance.setPopupLifetime(Duration.millis(120000));
 
-        new Thread(() -> {
-            while (true) {
-                try {
-                    final String message = recieveMSg();
-                    Platform.runLater(() -> instance.notifyError("crashed", message));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
-
-
-
-
+        instance.notifyInfo("god is grea", "ddddddddddd");
+        final ProgressBar progressBar = instance.progressBar;
+        progressBar.setDisable(true);
 
 
         instance.setOnNotificationPressed(event -> {
@@ -57,7 +48,7 @@ public class Noti extends Application {
     }
 
 
-    public static void sendmsg(String ip, int port, String msg) throws IOException {
+    void sendmsg(String ip, int port, String msg) throws IOException {
         DatagramSocket socket = new DatagramSocket();
         DatagramPacket packet = new DatagramPacket(msg.getBytes(), msg.length(), InetAddress.getByName(ip), (int) port);
         socket.send(packet);
