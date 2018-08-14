954e8e64609a672369a7a2e422a25b369fb7434e
abvaalgit <abfzlabfzl@gmail.com>
2018-08-14 05:16:54 +0430

    henzo socks!!!

---
+++ src/main/java/com/amin/ui/map/DB.java
@@ -0,0 +1,14 @@
+package com.amin.ui.map;
+
+import com.amin.analysis.Mapping;
+
+/**
+ * is created by aMIN on 8/13/2018 at 11:21 PM
+ */
+public class DB {
+
+    public static void main(String[] args) {
+//        Mapping.readFileLines("config/countries.configfile.conf")
+//        Mapping.LatLong.getLatLongForAContryCities(abspathfile)
+    }
+}
