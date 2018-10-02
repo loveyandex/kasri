315b0624a49b589f8e76e75e50ac12d72159acc7
abvaalgit <abfzlabfzl@gmail.com>
2018-10-02 19:26:00 +0330

    xxxx

--- src/main/java/com/amin/ui/Alert.java
+++ src/main/java/com/amin/ui/Alert.java
@@ -3,6 +3,14 @@ package com.amin.ui;
 import com.jfoenix.controls.JFXButton;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventType;
+import javafx.fxml.Initializable;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * is created by aMIN on 8/21/2018 at 5:02 AM
@@ -18,7 +26,13 @@ public class Alert  {
 
     }
 
+
+
+
+
     public void exit(ActionEvent actionEvent) {
         Platform.exit();
     }
+
+
 }
