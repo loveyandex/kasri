ed744dff377d302685da717aa535aab75bfea40d
abvaalgit <abfzlabfzl@gmail.com>
2018-10-07 06:48:07 +0330

    sd

--- src/main/java/com/amin/jni/HelloJNI.c
+++ src/main/java/com/amin/jni/HelloJNI.c
@@ -2,12 +2,12 @@
 #include <jni.h>        // JNI header provided by JDK
 #include <stdio.h>      // C Standard IO Header
 #include "com_amin_jni_HelloJNI.h"   // Generated
-#include "A.h"
+#include "headers/A.h"
 
 #define FILE_NAME "com/text.txt"
 // Implementation of the native method sayHello()
 JNIEXPORT void JNICALL Java_com_amin_jni_HelloJNI_sayHello(JNIEnv *env, jobject thisObj) {
-  FILE* file_ptr = fopen(FILE_NAME, "w");
+  FILE* file_ptr = fopen(FILE_NAME, "a");
           // write to file vs write to screen
   fprintf(file_ptr, "this is a test %d\n", 12); // write to file
   fclose(file_ptr);
