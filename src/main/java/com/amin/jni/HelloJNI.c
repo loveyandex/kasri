5b6b722b115c42151a41679c8ce00b5e0e438d27
abvaalgit <abfzlabfzl@gmail.com>
2018-10-07 06:40:08 +0330

    cleaning code

---
+++ src/main/java/com/amin/jni/HelloJNI.c
@@ -0,0 +1,21 @@
+// Save as "HelloJNI.c"
+#include <jni.h>        // JNI header provided by JDK
+#include <stdio.h>      // C Standard IO Header
+#include "com_amin_jni_HelloJNI.h"   // Generated
+#include "A.h"
+
+#define FILE_NAME "com/text.txt"
+// Implementation of the native method sayHello()
+JNIEXPORT void JNICALL Java_com_amin_jni_HelloJNI_sayHello(JNIEnv *env, jobject thisObj) {
+  FILE* file_ptr = fopen(FILE_NAME, "w");
+          // write to file vs write to screen
+  fprintf(file_ptr, "this is a test %d\n", 12); // write to file
+  fclose(file_ptr);
+   printf("Hello World!\n");
+    add(4, 6);
+    /*This calls add function written in myhead.h
+      and therefore no compilation error.*/
+    multiply(5, 5);
+
+   return;
+}
---
+++ src/main/java/com/amin/jni/HelloJNI.java
@@ -0,0 +1,20 @@
+package com.amin.jni;
+
+public class HelloJNI {  // Save as HelloJNI.java
+    static {
+        System.loadLibrary("hello"); // Load native library hello.dll (Windows) or libhello.so (Unixes)
+        //  at runtime
+        // This library contains a native method called sayHello()
+    }
+
+    // Declare an instance native method sayHello() which receives no parameter and returns void
+    private native void sayHello();
+
+    // Test Driver
+    public static void main(String[] args) {
+        new HelloJNI().sayHello();  // Create an instance and invoke the native method
+    }
+}
+
+//todo java -Djava.library.path=.\com\amin\jni com.amin.jni.HelloJNI
+//todo gcc -I "%JAVA_HOME%\include" -I "%JAVA_HOME%\include\win32" -shared -o hello.dll HelloJNI.c
