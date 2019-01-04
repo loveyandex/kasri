e8434812460936d9abd2f118ed1426a2d0602ee7
abvaalgit <abfzlabfzl@gmail.com>
2019-01-04 05:53:33 +0330

    d

---
+++ weathermether/test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from ecmwfapi import ECMWFDataServer
+    
+server = ECMWFDataServer()
+    
+server.retrieve({
+    'stream'    : "oper",
+    'levtype'   : "sfc",
+    'param'     : "167.128",
+    'repres'    : "gg",
+    'step'      : "0",
+    'time'      : "12",
+    'date'      : "1986-08-01/to/1986-08-31",
+    'dataset'   : "era15",
+    'type'      : "an",
+    'class'     : "er",
+    'target'    : "era15_1986-08-01to1986-08-31_12.grib"
+})
