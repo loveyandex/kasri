dd1c7b6887f442e2d7ee80f1a9c3e220c876822b
abvaalgit <abfzlabfzl@gmail.com>
2019-01-05 03:56:16 +0330

    dd

---
+++ src/test/java/com/amin/test/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.amin.test;// Hello World Applet
+// Save file as HelloWorld.java 
+
+import java.applet.Applet; 
+import java.awt.Graphics; 
+
+// HelloWorld class extends Applet 
+public class HelloWorld extends Applet 
+{ 
+	// Overriding paint() method 
+	@Override
+	public void paint(Graphics g) 
+	{ 
+		g.drawString("Hello World", 20, 20); 
+	} 
+	
+} 
