4ff31d4c495e7db24a6505e367816c9a73935dcd
aminabvaal <ag1992@aut.ac.ir>
2019-05-25 17:27:24 +0430

    yu

--- http-server/bin/http-server.cmd
+++ http-server/bin/http-server.cmd
@@ -1,5 +1,5 @@
-@IF EXIST "..\..\nodejs\node.exe" (
-  "..\..\nodejs\node.exe"  "..\nodejs\node_modules\http-server\bin\http-server" %*
+@IF EXIST "..\nodejs\node.exe" (
+  "..\nodejs\node.exe"  "..\nodejs\node_modules\http-server\bin\http-server" %*
 ) ELSE (
   @SETLOCAL
   @SET PATHEXT=%PATHEXT:;.JS;=;%
