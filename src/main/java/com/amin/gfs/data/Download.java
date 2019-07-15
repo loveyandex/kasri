aef6e7a02defeef10fd6e70ede88dbc03a50c755
aminabvaal <ag1992@aut.ac.ir>
2019-07-16 03:08:37 +0430

    great commit

---
+++ src/main/java/com/amin/gfs/data/Download.java
@@ -0,0 +1,93 @@
+package com.amin.gfs.data;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.util.ArrayList;
+
+/**
+ * created By aMIN on 7/15/2019 2:35 AM
+ */
+
+public class Download {
+
+    OkHttpClient client = new OkHttpClient();
+
+    String run(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        Element body = Jsoup.connect("https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_1p00.pl?dir=%2Fgfs.20190715%2F00").get().body();
+
+        Elements elementsByAttributeValue = body.getElementsByAttributeValue("type", "checkbox");
+
+        ArrayList<String> levels = new ArrayList<>();
+        ArrayList<String> vars = new ArrayList<>();
+
+        for (Element element : elementsByAttributeValue) {
+            String name = element.attr("name");
+            if (name.contains("lev_")) {
+                System.out.println(element.attr("name").replaceAll("_", " ").replaceFirst("lev", ""));
+                levels.add(name);
+            } else if (name.contains("var_")) {
+                System.err.println(element.attr("name").replaceAll("_", " ").replaceFirst("var", ""));
+                vars.add(name);
+
+            }
+        }
+
+//        System.exit(0);
+
+
+        String path0_18z = "https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_1p00.pl?file=gfs.t18z.pgrb2.1p00.f000";
+        String path0_12z = "https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_1p00.pl?file=gfs.t12z.pgrb2.1p00.f000";
+
+        String year = "2019";
+        String month = "07";
+        String day = "14";
+        String hour = "18";
+        String dir = "&dir=%2Fgfs."+year+month+day+"%2F"+hour;
+
+        String levv = "";
+        for (int i = 0; i < ((int) (Math.random() * 4)); i++) {
+            levv+="&"+levels.get(i)+"=on";
+        }
+        String varr = "";
+        for (int i = 0; i < ((int) (Math.random() * 4)); i++) {
+            varr+="&"+vars.get(i)+"=on";
+        }
+
+        System.out.println("levv "+levv);
+        System.out.println("varss "+varr);
+
+        String others = "&leftlon=0&rightlon=360&toplat=90&bottomlat=-90";
+
+
+        String spec = path0_18z + levv + varr + others + others+dir;
+
+        URL website = new URL(spec);
+
+        ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+        FileOutputStream fos = new FileOutputStream("kini.f000");
+        fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+    }
+}
