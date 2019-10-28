4a6237ebfa2d254f8149dfaf589acc3041e195e4
Lusics Mayrics <abbfzl@gmail.com>
2019-10-28 21:52:49 +0330

    master came back:))

--- src/main/java/com/amin/gfs/data/ConvertJsonUVT.java
+++ src/main/java/com/amin/gfs/data/ConvertJsonUVT.java
@@ -11,6 +11,8 @@ import java.util.Scanner;
 
 public class ConvertJsonUVT {
     public static void main(String[] args) throws IOException {
+
+
         File file = new File("nws-data/");
         for (File listFile : file.listFiles()) {
             String outo = "nws-json/" + listFile.getName() + ".json";
